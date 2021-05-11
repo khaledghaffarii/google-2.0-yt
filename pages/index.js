099b7a65f3ba6cbabbb49d6d6aaf88b769dead44
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-05-11 13:08:41 +0100

    style: formatean fix  body component in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import Avatar from "../components/Avatar";
-import { ViewGridIcon } from "@heroicons/react/solid";
+import { ViewGridIcon, MicrophoneIcon } from "@heroicons/react/solid";
+import { SearchIcon } from "@heroicons/react/outline";
 export default function Home() {
   return (
     <div>
@@ -26,13 +27,22 @@ export default function Home() {
         </div>
       </header>
 
-      <form className="p-10"> 
+      <form className="flex flex-col items-center pt-44 flex-grow ">
         <Image
           src="https://www.google.co.uk/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
           height={100}
           width={300}
-          
         />
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-lg rounded-full border px-5 py-3 items-center border-gray-200 sm:max-w-xl lg:max-w-2xl">
+          <SearchIcon className="h-4 mr-4 text-gray-500 " />
+          <input type="text" className="focus:outline-none flex-grow  " />
+          <MicrophoneIcon className="h-5" />
+        </div> 
+        <div className="flex">
+
+          <button className="btn"> Google Search</button>
+          <button className="btn"> I'm Felling Lucky</button>
+        </div>
       </form>
     </div>
   );
