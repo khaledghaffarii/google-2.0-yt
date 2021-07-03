ee581a98392727f11e9b67f69d2018ea2cff3400
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-07-04 00:09:59 +0100

    style:create and formate the Heder options

--- components/HeaderOption.js
+++ components/HeaderOption.js
@@ -1,9 +1,10 @@
-function HeaderOption() {
+function HeaderOption({Icon,selected,title}) {
     return (
-        <div>
-            
+        <div className={`flex items-center space-x-4 border-b-4 border-transparent mt-5 p-2 hover:text-blue-500 cursor-pointer hover:border-blue-500 pb-3 ${ selected && "text-blue-500 border-blue-500"}`}>
+            <Icon className="h-5"/>
+            <p className="hidden sm:inline-flex">{title} </p>
         </div>
-    )
+    );
 }
 
-export default HeaderOption
+export default HeaderOption;
--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -4,16 +4,21 @@ import {
   PlayIcon,
   PhotographIcon,
   NewspaperIcon,
-  MailIcon,
+  MapIcon,
   DotsVerticalIcon,
 } from "@heroicons/react/outline";
 function HeaderOptions() {
   return (
-    <div>
+    <div className="flex justify-evenly  w-full border-b-5 ">
       <div className="flex space-x-6">
-        <HeaderOption />
+        <HeaderOption Icon={SearchIcon} title="All" selected />
+        <HeaderOption Icon={PhotographIcon} title="Images" selected />
+        <HeaderOption Icon={PlayIcon} title="Video" selected />
+        <HeaderOption Icon={NewspaperIcon} title="New" selected />
+        <HeaderOption Icon={MapIcon} title="Maps" selected />{" "}
+        <HeaderOption Icon={DotsVerticalIcon} title="More" selected />
       </div>
-      <div className="flex space-x-4">
+      <div className=" flex space-x-4 mt-5 p-2   text-gray-500  ">
         <p className="link">Settings</p>
         <p className="link">Tools</p>
       </div>
