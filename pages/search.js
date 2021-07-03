84738ba3655ec6f6ed6f38f538af41c44704b034
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-07-03 11:48:31 +0100

    refactor: adding a Head component in Search page

--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,10 @@
+import Head from "next/head";
 function Search() {
     return (
         <div>
-            
+            <Head>
+                <title>Search Page</title>
+            </Head>
         </div>
     )
 }
