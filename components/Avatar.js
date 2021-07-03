b37bf5d3ab513d575992df1b8dd04e78f3d9e733
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-07-03 15:56:16 +0100

    refactor: converter the className at to function javascript

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function Avatar  ({url})  {
+function Avatar  ({url ,className})  {
     return <img
           loading="lazy" src={url}
-         className="rounded-full h-10 cursor-pointer animation transition duration-150 transform hover:scale-110"
-         alt="profile pic"
+         className={`rounded-full h-10 cursor-pointer animation transition duration-150 transform hover:scale-110"
+         alt="profile pic ${className}`}
       />;
     
 }
