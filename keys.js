7b965a924e7c4c652b6c6d67c8d98b6be16055ac
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-07-04 14:38:17 +0100

    refactor: chage keys' Api google

--- keys.js
+++ keys.js
@@ -3,4 +3,5 @@ export const API_KEY = "AIzaSyA83B1R6N1P7xP5d1v9Jmi9l3SKrjRuO8w";
 
 
 // Context Key - https://cse.google.com/sce/create/new
-export const CONTEXT_KEY = "4d4c9eff6a3c14086";
+// export const CONTEXT_KEY = "4d4c9eff6a3c14086";
+export const CONTEXT_KEY = "9867c733a42c0c643";
