46431f25a2d68c9b727a76216f6697a1a40c839f
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-07-04 10:16:35 +0100

    refactor:  create a Keys.js and import in the `getServerSideProps` function

---
+++ keys.js
@@ -0,0 +1,6 @@
+// API Key - https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = "AIzaSyA83B1R6N1P7xP5d1v9Jmi9l3SKrjRuO8w";
+
+
+// Context Key - https://cse.google.com/sce/create/new
+export const CONTEXT_KEY = "4d4c9eff6a3c14086";
--- pages/search.js
+++ pages/search.js
@@ -1,10 +1,13 @@
 import Head from "next/head";
 import Header from "../components/Header";
-function Search() {
+import { API_KEY, CONTEXT_KEY } from "../keys";
+function Search({results}) {
+    console.log(results)
     return (
         <div>
             <Head>
                 <title>Search Page</title>
+                <link ref="icon" href="/favicon.ico"/>
             </Head>
             <Header/>
         </div>
@@ -12,3 +15,18 @@ function Search() {
 }
 
 export default Search
+ 
+ export async function getServerSideProps(context){
+     const useDummyData = false;
+
+     const data = await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`).then(response => response.json() );
+    
+
+     //After the server has rendered .... Pass the results to the client ... 
+
+     return{
+         props:{
+             results: data
+         }
+     }
+ }
