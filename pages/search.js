6f6295728dee54c97cf57864d443b5b5fb938402
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-07-04 14:40:26 +0100

    refactor: change useDummyData value per false for use google Api in Search component

--- pages/search.js
+++ pages/search.js
@@ -23,7 +23,7 @@ function Search({ results }) {
 export default Search;
 
 export async function getServerSideProps(context) {
-  const useDummyData = true;
+  const useDummyData = false;
   const startIndex = context.query.start || "0";
 
   const data = useDummyData ? Response: await fetch(
