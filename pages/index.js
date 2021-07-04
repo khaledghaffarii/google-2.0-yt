874254ca7013fd0d7faa9ee7ae1da701016e8b03
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-07-04 15:40:24 +0100

    fix:correct the error flex in index component

--- pages/index.js
+++ pages/index.js
@@ -43,14 +43,14 @@ export default function Home() {
         </div>
       </header>
 
-      <form className=" flex flex-col items-center pt-44 flex-grow w-4/5 ">
+      <form className=" flex flex-col items-center mt-44 flex-grow w-4/5 ">
         <Image
           src="https://www.google.co.uk/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
           height={100}
           width={300}
         />
-        <div className="flex  w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-lg rounded-full border border-gray-200  px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
-          <SearchIcon className="h-4 mr-4 text-gray-500 " />
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-lg rounded-full border border-gray-200  px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
+          <SearchIcon className="h-5 mr-3 text-gray-500 " />
           <input
             ref={searchInputRef}
             type="text"
@@ -58,14 +58,10 @@ export default function Home() {
           />
           <MicrophoneIcon className="h-5" />
         </div>
-        <div className="flex  w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4  ">
-          <button className="btn " onClick={search}>
-            {" "}
-            Google Search
+        <div className="space-y-2 justify-center mt-8       sm:space-y-0 sm:flex-row sm:space-x-4">
+          <button className="btn" onClick={search}>Google Search
           </button>
-          <button className="btn " onClick={search}>
-            {" "}
-            I'm Felling Lucky
+          <button className="btn" onClick={search}>I'm Felling Lucky
           </button>
         </div>
       </form>
